11ae3dbc821e45cb22f55e42c59918f5b78281ff
VictorLago <victorprudentelago@outlook.com>
2022-10-02 00:51:52 -0300

    iniciando renovação do movimento

--- FishGame/Scripts/Peixe.gd
+++ FishGame/Scripts/Peixe.gd
@@ -4,35 +4,35 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var speed = 3.5
+var velocity = Vector2(0,0)
+var maxSpeed = 1
+var aceleration = 0.05
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_process(true)
+	look_at((position))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var mpos = get_global_mouse_position()
-	look_at(mpos)
+	draw_line(position, velocity, 1)
+	look_at(velocity)
 	
 func _physics_process(delta):
-	var move_direction = Vector2(0,0)
 	
 	var key_up = Input.is_action_pressed("ui_up")
 	var key_down = Input.is_action_pressed("ui_down")
 	var key_left = Input.is_action_pressed("ui_left")
 	var key_right = Input.is_action_pressed("ui_right")
-	print("oi")
-	if key_up:
-		move_direction.y = -speed
-	if key_down:
-		move_direction.y = speed
-	if key_right:
-		move_direction.x = speed
-	if key_left:
-		move_direction.x = -speed
-		
-	move_and_collide(move_direction)
+	if key_up and velocity.y > -maxSpeed:
+		velocity.y -= aceleration
+	if key_down and velocity.y < maxSpeed:
+		velocity.y += aceleration
+	if key_right and velocity.x < maxSpeed:
+		velocity.x += aceleration
+	if key_left and velocity.x > -maxSpeed:
+		velocity.x -= aceleration
+	move_and_collide(velocity)
 	
