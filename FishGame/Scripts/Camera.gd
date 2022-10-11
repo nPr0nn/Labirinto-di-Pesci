0141bc5b99b9692b800a287cc2c17facf96ea322
Lucas Nogueira Roberto <ra182553@cult.lab.ic.unicamp.br>
2022-10-11 10:03:01 -0300

    Ajusta limites de camera

--- FishGame/Scripts/Camera.gd
+++ FishGame/Scripts/Camera.gd
@@ -5,8 +5,21 @@ export (NodePath) onready var player = get_node(player)
 export (NodePath) onready var viewport_container = get_node(viewport_container)
 onready var real_camera_position     = global_position
 
+func trunc(x, a, b):
+	if x < a:
+		return a
+	if x > b:
+		return b
+	return x
+	
 func _process(delta):
-	var mouse_position  = get_viewport().get_mouse_position() / Game.window_scale - (Game.size - Vector2(10, 10))/2 + player.global_position
+	var mouse_position_on_screen = (get_viewport().get_mouse_position()/Game.window_scale)
+	mouse_position_on_screen.x = trunc(mouse_position_on_screen.x, 64, 192)
+	mouse_position_on_screen.y = trunc(mouse_position_on_screen.y, 36, 108)
+	var mouse_position  = mouse_position_on_screen - Game.size/2 + player.global_position
+	
+	print(mouse_position_on_screen)
+	
 	
 	var calculated_camera_position = lerp(player.global_position, mouse_position, 0.7)
 	real_camera_position = lerp(real_camera_position, calculated_camera_position, delta*5)
