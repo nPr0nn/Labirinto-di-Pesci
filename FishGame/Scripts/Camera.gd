a2b003c29043c701c836ba9fc87073c37d9dbe64
Lucas Nogueira Roberto <ra182553@students.ic.unicamp.br>
2022-11-06 16:12:23 -0300

    Feature/art and some animation

--- FishGame/Scripts/Camera.gd
+++ FishGame/Scripts/Camera.gd
@@ -7,12 +7,10 @@ onready var real_camera_position     = global_position
 
 func _process(delta):
 	var mouse_position_on_screen = (get_viewport().get_mouse_position()/Game.window_scale)
-	mouse_position_on_screen.x = clamp(mouse_position_on_screen.x, 64, 192)
-	mouse_position_on_screen.y = clamp(mouse_position_on_screen.y, 36, 108)
+#	mouse_position_on_screen.x = clamp(mouse_position_on_screen.x, 64*2, 192)
+#	mouse_position_on_screen.y = clamp(mouse_position_on_screen.y, 36*2, 108*2)
 	var mouse_position  = mouse_position_on_screen - Game.size/2 + player.global_position
-	
-	print(player.position)
-	
+
 	var calculated_camera_position = lerp(player.global_position, mouse_position, 0.7)
 	real_camera_position = lerp(real_camera_position, calculated_camera_position, delta*5)
 	var subpixel_camera_position = real_camera_position.round() - real_camera_position
